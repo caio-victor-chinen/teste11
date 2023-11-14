957471a0869c0177bf63360a4da294d301bcdf66
usrlabecon <usrlabecon@eniac.edu.br>
2023-11-14 18:23:56 -0300

    segundo commit

--- PHP/usuario.php
+++ PHP/usuario.php
@@ -7,7 +7,17 @@ require("usuario.php");
     $senha = $_POST["senha"];
 
 
-    $sql = ""
+    $sql = "SELECT * FROM cadastro (email, senha) VALUES ('$email', '$senha')";
+
+    if(mysqli_query($conn, $sql)){
+        echo "Login feito com Sucesso";
+    }else{
+        echo "Falha no login" . $stmt->error;
+    }
+
+    $conn->close();
+
+
 
 
 
